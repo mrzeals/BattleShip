7558082301ca966ba67c8fc0a3acfeca06eddb6d
alvinvoonyn <alvinvoonyn@yahoo.com>
2017-05-14 14:21:42 +0800

    Apply the draw destoryed ships function

--- Battleships/src/DiscoveryController.cs
+++ Battleships/src/DiscoveryController.cs
@@ -79,10 +79,18 @@ internal static class DiscoveryController
 		UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
 		UtilityFunctions.DrawMessage();
 
+		//Added by Voon.
+		//Draw Destroyed Ships.
+		UtilityFunctions.DrawDestroyField(GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, true);
+
 		SwinGame.DrawText(GameController.HumanPlayer.Shots.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SHOTS_TOP);
 		SwinGame.DrawText(GameController.HumanPlayer.Hits.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, HITS_TOP);
 		SwinGame.DrawText(GameController.HumanPlayer.Missed.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
 
+		//Added by Voon.
+		//Draw the number of ships left that need to destroy.
+		SwinGame.DrawText("Number of Ships left: ", Color.White, GameResources.GameFont("Menu"), 350, 90);
+		SwinGame.DrawText((5 - GameController.ComputerPlayer.PlayerGrid.ShipsKilled).ToString(), Color.Yellow, GameResources.GameFont("Menu"), 515, 90);
 
 		//added by jesse -- gaming page with home button to return home page
 		SwinGame.DrawBitmap(GameResources.GameImage("HomeButton"), 685, 70);
